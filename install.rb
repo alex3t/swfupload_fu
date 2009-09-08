e80457dd4424ebc018f843155e4f8a2cc261c907
Alexander Tretyakov <alex3t@macbook-pro-alexander-tretyakov.local>
2009-09-08 13:05:48 +0800

    Install fix

--- install.rb
+++ install.rb
@@ -22,8 +22,8 @@ Swfupload::InstallHelpers::copy_files 'javascripts',["swfupload.js", "swfupload.
 Swfupload::InstallHelpers::copy_files 'stylesheets',["swfupload.css"]
 Swfupload::InstallHelpers::copy_files 'images',["FullyTransparent_65x29.png"]
 Swfupload::InstallHelpers::copy_files 'flash',["swfupload.swf"]
-Swfupload::InstallHelpers::copy_files '',["_swfupload_form.html.erb"],File.join('app','views','shared')
-Swfupload::InstallHelpers::copy_files '',["session_flash_support.rb"],File.join('config','initializers')
+Swfupload::InstallHelpers::copy_files 'app/views/shared',["_swfupload_form.html.erb"],''
+Swfupload::InstallHelpers::copy_files 'config/initializers',["session_flash_support.rb"],''
 rescue Exception => e
 	puts "e=#{e}"
 end
