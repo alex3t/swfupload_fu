25b4c7ec1d6444283af8aa764c040a932296871c
Alexander Tretyakov <alex3t@macbook-pro-alexander-tretyakov.local>
2009-09-08 12:48:33 +0800

    Install fix

--- install.rb
+++ install.rb
@@ -1,17 +1,8 @@
 require 'fileutils'
-
-puts "Copy started..."
-copy_files 'javascripts',["swfupload.js", "swfupload.fileprogress.js", "swfupload.handlers.js", "swfupload.queue.js", "swfupload-settings.js"]
-#copy_files 'stylesheets',["swfupload.css"]
-#copy_files 'images',["FullyTransparent_65x29.png"]
-#copy_files 'flash',["swfupload.swf"]
-#copy_files 'app/views',["_swfupload_form.html.erb"],File.join('app','views','shared')
-#copy_files 'config/initializers',["session_flash_support.rb"],File.join('config','initializers')
-
-#puts "Files copied - Installation complete!"
-
-
-def copy_files dir, files, rails_folder="public"
+RAILS_ROOT = File.join(File.dirname(__FILE__), '..', '..', '..')
+module Swfupload
+	module InstallHelpers
+def self.copy_files dir, files, rails_folder="public"
   files.each do |js_file|
     dest_dir = File.join(RAILS_ROOT, rails_folder,dir)
     FileUtils.mkdir(dest_dir) unless File.exist? dest_dir
@@ -21,3 +12,17 @@ def copy_files dir, files, rails_folder="public"
     p "copy #{dest_file}"
   end  
 end
+end
+end
+
+
+puts "Copy started..."
+Swfupload::InstallHelpers::copy_files 'javascripts',["swfupload.js", "swfupload.fileprogress.js", "swfupload.handlers.js", "swfupload.queue.js", "swfupload-settings.js"]
+#copy_files 'stylesheets',["swfupload.css"]
+#copy_files 'images',["FullyTransparent_65x29.png"]
+#copy_files 'flash',["swfupload.swf"]
+#copy_files 'app/views',["_swfupload_form.html.erb"],File.join('app','views','shared')
+#copy_files 'config/initializers',["session_flash_support.rb"],File.join('config','initializers')
+
+#puts "Files copied - Installation complete!"
+
